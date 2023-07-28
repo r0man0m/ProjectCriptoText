aec5a61893d8077b128280935dbebd50fec2022d
Roman Mudrud <Mudrudr@gmail.com>
2023-07-28 17:19:44 +0300

    Change dictionary

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,39 +1,61 @@
 import java.awt.*;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 public class BruteForce {
-
-    public static void force (String text, int sizeKeys) throws IOException {
+    public static void force (String text, int sizeKeys, int sizeAlphabet, Path path, char firstLetter) throws IOException {
         boolean isFind = false;
         int count = 0;
         int key = 0;
-        Path pathDictionary = Paths.get("C:\\Users\\Roman\\IdeaProjects\\ProjectCriptoText\\dictionary.txt");
-        List<String> listDictionary = Files.readAllLines(pathDictionary);
-        for (int i = 1; i <= sizeKeys; i++) {
-            Encryption obj = new Encryption(i);
-            String [] arrText = obj.decryptionMethod(text).split(" ");
-            for (String S: arrText
-                 ) {
-                for (int j = 1; j < listDictionary.size(); j++) {
-                    if (listDictionary.get(j).contains(S)) {
-                        count++;
+        char first = firstLetter;
+        int s = sizeAlphabet;
+        int keys = sizeKeys;
+        String encryptText = text;
+        String[] dictArray;
+        String[]arr;
+        Dictionary dictionary = new Dictionary(s, path, first);
+        dictionary.setDictionary();
+        for (int i = 1; i < keys; i++) {
+            if(count > 3) {
+                key = keys;
+                isFind = true;
+                break;
+            }else {
+                Encryption obj = new Encryption(i);
+                arr = new String[obj.decryptionMethod(text).length()];
+                arr = obj.decryptionMethod(encryptText).split(" ");
+                for (int m = 0; m < arr.length; m++) {
+                    if(arr[m].length() < 2) {
+                        arr[m] = "";
                     }
                 }
-                    if(count > 5) {
-                        isFind = true;
-                        key = i;
-                        break;
+                String dictString = "";
+                for (int j = 0; j < s; j++) {
+                   Iterator<String> iterator = dictionary.getDictionary(first).listIterator();
+                   while (iterator.hasNext()) {
+                        dictString = dictString + " " + iterator.next();
+                   }
+                   dictArray = new String[dictionary.getDictionary(first).size()];
+                   dictArray = dictString.split(" ");
+                    for (int k = 0; k < arr.length; k++) {
+                        for (int l = 0; l < dictArray.length; l++) {
+                            if(arr[k].equalsIgnoreCase(dictArray[l])) {
+                                count++;
+                            }
+                        }
                     }
-            }
-            if(isFind) {
+                    first++;
+                }
 
-                System.out.println(new Encryption(key).decryptionMethod(text));
-                break;
+
             }
+
+        }
+        if(isFind) {
+            System.out.println(new Encryption(key).decryptionMethod(encryptText));
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Path path = Paths.get("C:\\Tests\\dictionary.txt");
+        int size;
         System.out.println("Enter key");
         Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
         int key = scanner.nextInt();
+        String firstLetterString;
         Path textPath = Paths.get("C:\\Users\\Roman\\IdeaProjects\\ProjectCriptoText\\Text.txt");
         String textString = Files.readString(textPath);
         Encryption object = new Encryption(key);
@@ -21,6 +26,12 @@ public class Main {
         System.out.println("Brute Force Method");
         System.out.println("Enter maximum volume of key");
         key = scanner.nextInt();
-        BruteForce.force(object.encryptionMethod(textString),key );
+        System.out.println("Enter size of alphabet");
+        size = scanner.nextInt();
+        System.out.println("Enter first letter of alphabet");
+        firstLetterString = scanner1.nextLine();
+        BruteForce.force(object.encryptionMethod(textString),key, size, path, firstLetterString.charAt(0));
+
+
     }
 }
